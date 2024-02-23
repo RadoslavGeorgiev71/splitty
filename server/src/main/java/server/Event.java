05a80a54fdfa04d4bf95d9f12d5aead80ce6f864
Alexander Oey <A.Y.H.Oey@student.tudelft.nl>
2024-02-23 14:08:09 +0100

    Event class with general structure

---
+++ server/src/main/java/server/Event.java
@@ -0,0 +1,127 @@
+package server;
+
+import java.util.Objects;
+
+public class Event {
+
+    private String title;
+    private String inviteCode;
+    private Participant creator;
+    private List<Participant> participants;
+    private List<Expense> expenses;
+    private String dateTime;
+    private int id;
+    private Activity lastActivity;
+
+    public Event(String title, String inviteCode, Participant creator, List<Participant> participants,
+                 List<Expense> expenses, String dateTime, int id, Activity lastActivity) {
+        this.title = title;
+        this.inviteCode = inviteCode;
+        this.creator = creator;
+        this.participants = participants;
+        this.expenses = expenses;
+        this.dateTime = dateTime;
+        this.id = id;
+        this.lastActivity = lastActivity;
+    }
+
+    public int getTotal() {
+        int totalExpenses = 0;
+        for(int i = 0; i < expenses.size(); i++) {
+            totalExpenses = totalExpenses + expenses.get(i).getAmount();
+        }
+        return totalExpenses;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public Participant getCreator() {
+        return creator;
+    }
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Activity getLastActivity() {
+        return lastActivity;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public void setCreator(Participant creator) {
+        this.creator = creator;
+    }
+
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    public void removeParticipant(Participant participant) {
+        for(int i = 0; i < participants.size(); i++) {
+            if(participants.get(i).equals(participant)) {
+                participants.remove(i);
+            }
+        }
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    public void removeExpense(Expense expense) {
+        for(int i = 0; i < expenses.size(); i++) {
+            if(expenses.get(i).equals(expense)) {
+                expenses.remove(i);
+            }
+        }
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLastActivity(Activity lastActivity) {
+        this.lastActivity = lastActivity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event event)) return false;
+        return getId() == event.getId() && Objects.equals(getTitle(), event.getTitle()) && Objects.equals(getInviteCode(), event.getInviteCode()) && Objects.equals(getCreator(), event.getCreator()) && Objects.equals(getParticipants(), event.getParticipants()) && Objects.equals(getExpenses(), event.getExpenses()) && Objects.equals(getDateTime(), event.getDateTime()) && Objects.equals(getLastActivity(), event.getLastActivity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getInviteCode(), getCreator(), getParticipants(), getExpenses(), getDateTime(), getId(), getLastActivity());
+    }
+}
