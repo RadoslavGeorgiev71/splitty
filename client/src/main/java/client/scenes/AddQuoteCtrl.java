772c81620eb921e224d560ba8e5bccc6c7702014
Alexandru Theodor Tarcuta <A.T.Tarcuta@student.tudelft.nl>
2024-02-24 16:18:49 +0100

    Started work on making template project adhere to checkstyle rules

--- client/src/main/java/client/scenes/AddQuoteCtrl.java
+++ client/src/main/java/client/scenes/AddQuoteCtrl.java
@@ -41,6 +41,11 @@ public class AddQuoteCtrl {
     @FXML
     private TextField quote;
 
+    /**
+     *
+     * @param server
+     * @param mainCtrl
+     */
     @Inject
     public AddQuoteCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
@@ -48,11 +53,17 @@ public class AddQuoteCtrl {
 
     }
 
+    /**
+     *
+     */
     public void cancel() {
         clearFields();
         mainCtrl.showOverview();
     }
 
+    /**
+     *
+     */
     public void ok() {
         try {
             server.addQuote(getQuote());
@@ -69,6 +80,10 @@ public class AddQuoteCtrl {
         mainCtrl.showOverview();
     }
 
+    /**
+     *
+     * @return
+     */
     private Quote getQuote() {
         var p = new Person(firstName.getText(), lastName.getText());
         var q = quote.getText();
@@ -81,16 +96,20 @@ public class AddQuoteCtrl {
         quote.clear();
     }
 
+    /**
+     *
+     * @param e
+     */
     public void keyPressed(KeyEvent e) {
         switch (e.getCode()) {
-        case ENTER:
-            ok();
-            break;
-        case ESCAPE:
-            cancel();
-            break;
-        default:
-            break;
+            case ENTER:
+                ok();
+                break;
+            case ESCAPE:
+                cancel();
+                break;
+            default:
+                break;
         }
     }
 }
