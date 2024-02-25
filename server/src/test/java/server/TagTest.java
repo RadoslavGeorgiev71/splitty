22ab5ce205c7c6d1b13cfe90cb6052b5395dab24
Radoslav Georgiev <R.K.Georgiev@student.tudelft.nl>
2024-02-25 21:49:28 +0100

    Added TagTest.java class to test the basic methods of Tag.java

---
+++ server/src/test/java/server/TagTest.java
@@ -0,0 +1,53 @@
+package server;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TagTest {
+    Tag t1, t2, t3;
+    @BeforeEach
+    void testBasis() {
+        t1 = new Tag("food", "red");
+        t2 = new Tag("drinks", "blue");
+        t3 = new Tag("drinks", "blue");
+    }
+
+    @Test
+    void testConstructor() {
+        assertNotNull(t1);
+    }
+
+    @Test
+    void testGetType() {
+        assertEquals("food", t1.getType());
+    }
+
+    @Test
+    void testGetColor() {
+        assertEquals("red", t1.getColor());
+    }
+
+    @Test
+    void testSetType() {
+        t1.setType("decoration");
+        assertEquals("decoration", t1.getType());
+    }
+
+    @Test
+    void testSetColor() {
+        t1.setColor("purple");
+        assertEquals("purple", t1.getColor());
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(t2, t3);
+        assertNotEquals(t1, t2);
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(t2.hashCode(), t3.hashCode());
+    }
+}
