00779c6e018bb19b8054f9b0487cdc77ac4337fd
Alexander Oey <A.Y.H.Oey@student.tudelft.nl>
2024-02-25 19:38:49 +0100

    Added some methods and changed some of the formatting, more info in description of Merge Request

--- server/src/main/java/server/Event.java
+++ server/src/main/java/server/Event.java
@@ -1,7 +1,10 @@
 package server;
 
-import java.util.List;
 import java.util.Objects;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Event {
 
@@ -13,81 +16,48 @@ public class Event {
     private String dateTime;
     private int id;
 
+
+    // 1.   Participant and Expense Class appearing as an error is because they are placeholders,
+    //      as I can't access the Person and Expense classes yet.
+    // 2.   Not sure how to format the Invite Code yet, but I'm guessing that will be done in the Event class.
+    // 3.   Same as above for the ID.
+    // 4.   Will later add a correct automatic dateTime update method, which will run when an "edit" method is used.
+    /**
+     * Constructor for the Event class
+     * @param title Title used to differentiate the different Events
+     * @param inviteCode Unique code used to join an Event
+     * @param creator Participant who created the Event
+     * @param participants Participants who participate in the Event
+     * @param expenses Expenses made during the Event
+     */
     public Event(String title, String inviteCode, Participant creator, List<Participant> participants,
-                 List<Expense> expenses, String dateTime, int id) {
+                 List<Expense> expenses) {
         this.title = title;
         this.inviteCode = inviteCode;
         this.creator = creator;
         this.participants = participants;
         this.expenses = expenses;
-        this.dateTime = dateTime;
-        this.id = id;
+        this.id = 0;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        this.dateTime = dtf.format(now);
     }
 
-    public int getTotal() {
-        int totalExpenses = 0;
-        for(int i = 0; i < expenses.size(); i++) {
-            totalExpenses = totalExpenses + expenses.get(i).getAmount();
-        }
-        return totalExpenses;
+    // Not sure if this should be put in the Expense class or in the Event class, as an Expense has to be created with
+    // all the supporting fields. I'm guessing this will just call the method to create an Expense.
+
+    /**
+     * Method to add an Expense to the Event
+     */
+    public void addExpense() {
+
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getInviteCode() {
-        return inviteCode;
-    }
-
-    public Participant getCreator() {
-        return creator;
-    }
-
-    public List<Participant> getParticipants() {
-        return participants;
-    }
-
-    public List<Expense> getExpenses() {
-        return expenses;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setInviteCode(String inviteCode) {
-        this.inviteCode = inviteCode;
-    }
-
-    public void setCreator(Participant creator) {
-        this.creator = creator;
-    }
-
-    public void setParticipants(List<Participant> participants) {
-        this.participants = participants;
-    }
-
-    public void removeParticipant(Participant participant) {
-        for(int i = 0; i < participants.size(); i++) {
-            if(participants.get(i).equals(participant)) {
-                participants.remove(i);
-            }
-        }
-    }
-
-    public void setExpenses(List<Expense> expenses) {
-        this.expenses = expenses;
-    }
-
+    /**
+     * Method to remove an Expense from the Event
+     * @param expense
+     */
     public void removeExpense(Expense expense) {
         for(int i = 0; i < expenses.size(); i++) {
             if(expenses.get(i).equals(expense)) {
@@ -96,20 +66,178 @@ public class Event {
         }
     }
 
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Method to find the Last Activity added to the Event
+     * @return the last Activity added to the Event
+     */
     public String getLastActivity() {
         int lastIndex = expenses.size() - 1;
         return expenses.get(lastIndex).getActivity();
     }
 
+    /**
+     * Method to calculate the total expenses of the Event
+     * @return total sum of Expenses
+     */
+    public int getTotal() {
+        int totalExpenses = 0;
+        for(int i = 0; i < expenses.size(); i++) {
+            totalExpenses = totalExpenses + expenses.get(i).getAmount();
+        }
+        return totalExpenses;
+    }
 
+    // Not sure if this should be put in the Participant class or in the Event class. Same issue as with the addExpense
+    // method.
+
+    /**
+     * Method to add a Participant to the Event
+     */
+    public void addParticipant() {
+
+    }
+
+    // Might be better to assign a number to every participant when they get added, so it's easier to remove
+    // the correct participant.
+
+    /**
+     * Method to remove a Participant from the Event
+     * @param participant
+     */
+    public void removeParticipant(Participant participant) {
+        for(int i = 0; i < participants.size(); i++) {
+            if(participants.get(i).equals(participant)) {
+                participants.remove(i);
+            }
+        }
+    }
+
+    /**
+     * Method to manually update the time when the Event was last updated
+     */
+    public void updateDateTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        dateTime = dtf.format(now);
+    }
+
+    /**
+     * Getter for the Title
+     * @return title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Setter for the Title
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Getter for the Invite Code
+     * @return inviteCode
+     */
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    /**
+     * Setter for the Invite Code
+     * @param inviteCode
+     */
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    /**
+     * Getter for the Creator
+     * @return creator
+     */
+    public Participant getCreator() {
+        return creator;
+    }
+
+    /**
+     * Setter for the Creator
+     * @param creator
+     */
+    public void setCreator(Participant creator) {
+        this.creator = creator;
+    }
+
+    /**
+     * Getter for the Participants
+     * @return participants
+     */
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    /**
+     * Setter for the Participants
+     * @param participants
+     */
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    /**
+     * Getter for the Expenses
+     * @return expenses
+     */
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    /**
+     * Setter for the Expenses
+     * @param expenses
+     */
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    /**
+     * Getter for the dateTime
+     * @return dateTime
+     */
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    /**
+     * Setter for the dateTime
+     * @param dateTime
+     */
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    /**
+     * Getter for the ID
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Setter for the ID
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Equals method for Event
+     * @param o
+     * @return The boolean equality of this with Object o
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -122,6 +250,10 @@ public class Event {
                 Objects.equals(getDateTime(), event.getDateTime());
     }
 
+    /**
+     * HashCode method for Event
+     * @return the hashCode of this
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getTitle(), getInviteCode(), getCreator(), getParticipants(),
