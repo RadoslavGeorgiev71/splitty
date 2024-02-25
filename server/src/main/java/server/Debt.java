a7d5544b2e976709d127975304d318b2f0a2936c
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-02-25 12:22:26 +0100

    Added all fields to Debt class

--- server/src/main/java/server/Debt.java
+++ server/src/main/java/server/Debt.java
@@ -3,5 +3,10 @@ package server;
 import java.time.LocalDateTime;
 
 public class Debt {
-   
+    private String id;
+    private Participant personOwed;
+    private Participant personPaying;
+    private double amount;
+    private boolean paid;
+    private LocalDateTime paidDateTime;
 }
