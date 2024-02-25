5c64800bf871e2dee11e422c430a8ac7d5c99ddc
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-02-25 13:19:09 +0100

    Added some unit test for now, can't run anything until Participant class is completed

---
+++ server/src/test/java/server/DebtTest.java
@@ -0,0 +1,73 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DebtTest {
+    Debt d1 = new Debt(123, null, null, 500);
+    Debt d2 = new Debt(123, null, null, 500);
+
+    /*
+    Will have to update some tests once Participant class gets added,
+    and will also have to add tests pertaining to Particpant fields
+     */
+    @Test
+    void testConstructor(){
+        assertNotNull(d1);
+    }
+
+    @Test
+    void testGetId() {
+        assertEquals(123, d1.getId());
+    }
+
+    @Test
+    void testGetAmount() {
+        assertEquals(500, d1.getAmount());
+    }
+
+    @Test
+    void testIsPaid() {
+        assertFalse(d1.isPaid());
+    }
+
+    @Test
+    void testGetPaidDateTimeNULL() {
+        assertEquals(d1.getPaidDateTime(), LocalDateTime.MIN);
+    }
+
+    @Test
+    void testSetId() {
+        d2.setId(321);
+        assertEquals(d2.getId(), 321);
+    }
+
+    @Test
+    void testSetAmount() {
+        d2.setAmount(4000);
+        assertEquals(d2.getAmount(), 4000);
+    }
+
+    @Test
+    void testSetPaid() {
+        d2.setPaid(true);
+        assertTrue(d2.isPaid());
+    }
+
+    @Test
+    void testSetPaidDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+        d2.setPaidDateTime(now);
+        assertNotNull(d2.getPaidDateTime());
+    }
+
+    @Test
+    void testGetPaidDateTimeNotNull() {
+        LocalDateTime now = LocalDateTime.now();
+        d2.setPaidDateTime(now);
+        assertEquals(now, d2.getPaidDateTime());
+    }
+}
