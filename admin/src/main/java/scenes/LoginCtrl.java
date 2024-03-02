500ed9d31ebb2a3da94fc5c4f753ae8cc99b1e66
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-03-02 22:13:55 +0100

    Created FXML for admin login, implemented basic controller skeleton

--- admin/src/main/java/scenes/LoginCtrl.java
+++ admin/src/main/java/scenes/LoginCtrl.java
@@ -1,4 +1,25 @@
 package scenes;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
 public class LoginCtrl {
+
+    @FXML
+    private Label errorID;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
 }
