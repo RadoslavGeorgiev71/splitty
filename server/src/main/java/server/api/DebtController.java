e4a0f2258b93f68415cf3e06d2ac62a13e7e68e8
Radoslav Georgiev <R.K.Georgiev@student.tudelft.nl>
2024-03-03 14:53:05 +0100

    Fixed checkstyle for DebtController.java

--- server/src/main/java/server/api/DebtController.java
+++ server/src/main/java/server/api/DebtController.java
@@ -12,20 +12,46 @@ import java.util.List;
 public class DebtController {
     private final DebtRepository repo;
 
+    /**
+     * Constructor for the controller
+     * @param repo - type DebtRepository which extends JpaRepository
+     */
     public DebtController(DebtRepository repo) {
         this.repo = repo;
     }
 
+    /**
+     * Get all debts
+     * @return all debts
+     */
     @GetMapping("/")
     public List<Debt> getAll() {
         return repo.findAll();
     }
 
+    /**
+     * Save a new debt if it is valid and returns a response
+     * @param debt - the debt to be saved
+     * @return response entity with "ok" status message if it is successful,
+     * "bad request" message otherwise
+     */
     @PostMapping("/")
     public ResponseEntity<Debt> add(@RequestBody Debt debt) {
+        if (debt.getPersonPaying() == null ||
+            debt.getPersonOwed() == null || debt.isPaid()) {
+            return ResponseEntity.badRequest().build();
+        }
 
+        Debt saved = repo.save(debt);
+        return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Returns a debt by its id if it exists
+     * @param id - the id to be searched with
+     * @return response with either "ok" or "bad request"
+     * response message
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Debt> getById(@PathVariable("id") int id) {
         if (id < 0 || !repo.existsById(id)) {
