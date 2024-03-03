2b1465740ebbf1c3b9dbe6e5ad63721df38a9ec1
Radoslav Georgiev <R.K.Georgiev@student.tudelft.nl>
2024-03-03 13:33:28 +0100

    Changes to OpenDebtsCtrl.java and OpenDebts.fxml

--- client/src/main/java/client/scenes/OpenDebtsCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsCtrl.java
@@ -3,6 +3,7 @@ package client.scenes;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +11,8 @@ import java.util.ResourceBundle;
 public class OpenDebtsCtrl implements Initializable {
     @FXML
     public Label first_debt;
+    @FXML
+    public TitledPane first_debt_1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
