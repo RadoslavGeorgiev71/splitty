989b94eab7b04fe7ca6c02fac4ff70d88d185d7c
Martim de Assis Lopes Cardeira <M.D.A.LopesCardeira@student.tudelft.nl>
2024-03-07 15:43:45 +0100

    Established the two endpoints for admin, no functionality yet

--- server/src/main/java/server/api/AdminController.java
+++ server/src/main/java/server/api/AdminController.java
@@ -1,9 +1,23 @@
 package server.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import commons.Quote;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.database.EventRepository;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
+    private String previousPassword;
+
+    @GetMapping(path = {"", "/"})
+    public void generatePassword(){
+
+    }
+
+    @PostMapping(path = { "", "/" })
+    public boolean authenticatePassword(@RequestBody String password) {
+
+        return true;
+    }
 }
