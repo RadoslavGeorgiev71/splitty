d7733452848da150e7f064cc67f47bf49fc8cea5
Alexander Oey <a.y.h.oey@student.tudelft.nl>
2024-03-11 00:05:06 +0100

    Added a back button, tab which shows all Expenses and a way to see all participating participants

--- client/src/main/java/client/scenes/EventOverviewCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewCtrl.java
@@ -5,8 +5,10 @@ import com.google.inject.Inject;
 import commons.Event;
 import commons.Participant;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.util.StringConverter;
 import javafx.collections.FXCollections;
 
@@ -18,6 +20,18 @@ public class EventOverviewCtrl {
 
     @FXML
     private ChoiceBox participantsMenu;
+
+    @FXML
+    private GridPane tabPaneAllGridPane;
+
+    @FXML
+    private ListView tabPaneFromPersonListView;
+
+    @FXML
+    private ListView tabPaneIncludingPersonListView;
+
+    @FXML
+    private Text ParticipatingParticipants;
 
     private Event event;
 
@@ -47,14 +61,74 @@ public class EventOverviewCtrl {
 
     @FXML
     public void onAddExpenseClick() {
+        // Doesn't work
+        mainCtrl.showAdd();
     }
 
     @FXML
     public void onSettleDebtsClick() {
     }
 
+    @FXML
+    public void onBackClick() {
+        mainCtrl.showOverview();
+    }
+
+    @FXML
+    public void tabPaneAllClick() {
+        tabPaneAllGridPane.getChildren().clear();
+        tabPaneAllGridPane.setVgap(10);
+        tabPaneAllGridPane.setHgap(75);
+        if(event != null) {
+            for (int i = 0; i < event.getExpenses().size(); i++) {
+                Label dateLabel = new Label("dateTime");
+                Label nameLabel = new Label("expense creator");
+                Button editButton = new Button("Edit");
+                dateLabel.setWrapText(true);
+                dateLabel.setWrapText(true);
+                dateLabel.setWrapText(true);
+
+                //editButton.setOnAction()
+
+                tabPaneAllGridPane.add(dateLabel, 0, i);
+                tabPaneAllGridPane.add(nameLabel, 1, i);
+                tabPaneAllGridPane.add(editButton, 2, i);
+            }
+        }
+    }
+
+    @FXML
+    public void tabPaneFromPersonClick() {
+        tabPaneFromPersonListView.getItems().clear();
+    }
+
+    @FXML
+    public void tabPaneIncludingPersonClick() {
+        tabPaneIncludingPersonListView.getItems().clear();
+    }
+
+    /**
+     * Sets the event
+     * @param event
+     */
     public void setEvent(Event event){
         this.event = event;
+    }
+
+    /**
+     * Sets the text that displays all the participants in the event
+     */
+    public void participatingParticipants() {
+        String participantsText = "";
+        if(event != null) {
+            for (int i = 0; i < event.getParticipants().size(); i++) {
+                participantsText += event.getParticipants().get(i).getName();
+                if(i < event.getParticipants().size() - 1) {
+                    participantsText += ", ";
+                }
+            }
+        }
+        ParticipatingParticipants.setText(participantsText);
     }
 
     public void initialize(){
@@ -76,5 +150,7 @@ public class EventOverviewCtrl {
             participantsMenu.getSelectionModel().selectFirst();
         }
 
+        tabPaneAllClick();
+        participatingParticipants();
     }
 }
