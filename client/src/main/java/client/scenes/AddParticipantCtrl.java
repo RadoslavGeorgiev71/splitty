2efa186695f5d576c2b7b8949e7dbbf54776f7be
Alexander Oey <a.y.h.oey@student.tudelft.nl>
2024-03-19 15:59:32 +0100

    Added AddParticipantCtrl.java and AddParticipant.fxml, with the required changes/methods in other classes

--- client/src/main/java/client/scenes/AddParticipantCtrl.java
+++ client/src/main/java/client/scenes/AddParticipantCtrl.java
@@ -5,8 +5,6 @@ import commons.Event;
 import commons.Participant;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
