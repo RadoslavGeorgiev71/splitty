a9b229f7353e70dfe17e63d919233a5d0e777252
Maria Burlacu <m.c.burlacu@student.tudelft.nl>
2024-03-28 15:32:39 +0100

    grid change

---
+++ server/src/test/java/server/Services/ExpenseServiceTest.java
@@ -0,0 +1,40 @@
+package server.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void testCreate() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void existsById() {
+    }
+
+    @Test
+    void findByEventId() {
+    }
+}
