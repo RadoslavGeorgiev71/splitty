ec64aa7a1226bb338f4658914f7b23428eb77b6d
Radoslav Georgiev <R.K.Georgiev@student.tudelft.nl>
2024-03-29 19:43:56 +0100

    Fixed IllegalStateException by making only the JavaFX application thread updating UI components.

--- client/src/main/java/client/scenes/EventOverviewCtrl.java
+++ client/src/main/java/client/scenes/EventOverviewCtrl.java
@@ -5,6 +5,7 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.Event;
 import commons.Participant;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -400,7 +401,7 @@ public class EventOverviewCtrl {
 
         server.registerEventUpdate(event -> {
             this.event = server.getEvent(event.getId());
-            this.initialize();
+            Platform.runLater(this::initialize);
         });
     }
 
