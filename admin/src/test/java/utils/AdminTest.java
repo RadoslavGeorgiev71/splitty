6136ad761521dce67a606ad21c866dfb3b053937
Martim de Assis Lopes Cardeira <M.D.A.LopesCardeira@student.tudelft.nl>
2024-04-01 12:54:55 +0200

    for now trivial methods are tested in admin, websocket now longer throws problems

---
+++ admin/src/test/java/utils/AdminTest.java
@@ -0,0 +1,60 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+import org.springframework.messaging.simp.stomp.StompSession;
+
+class AdminTest {
+
+    @Mock
+    Client mockClient;
+
+    @Mock
+    WebTarget mockWebTarget;
+
+    @Mock
+    Invocation.Builder mockBuilder;
+
+    @Mock
+    Response mockResponse;
+    @Mock
+    StompSession mockStompSession;
+
+    Admin admin;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        admin = new Admin("http://mocked-server/", mockStompSession);
+    }
+
+    @Test
+    void setPassword() {
+        admin.setPassword("123");
+        assertEquals("123", admin.getPassword());
+    }
+
+    @Test
+    void getPassword() {
+        assertEquals(admin.getPassword(), "none set");
+    }
+
+    @Test
+    void setURL() {
+        admin.setURL("http://www.google.com");
+        assertEquals("http://www.google.com", admin.getURL());
+    }
+
+    @Test
+    void getURL() {
+        assertEquals("http://mocked-server/", admin.getURL());
+    }
+}
